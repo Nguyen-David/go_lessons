af6911363b9808c48868fded7e8c16f75f228ad9
devidan <devidan@syneforge.com>
2023-02-20 00:50:50 +0200

    add book controller

---
+++ library_api/controllers/book_controller.go
@@ -0,0 +1,63 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"library_api/entities"
+	"library_api/repositories"
+	"net/http"
+	"sort"
+)
+
+func CreateBooks(w http.ResponseWriter, r *http.Request) { // TODO discuss about create action
+
+	books := []entities.Book{
+		{"Rage", "Stephen King", 1977},
+		{"Philosopher's Stone", "J. K. Rowling", 1997},
+		{"All Quiet on the Western Front", "Erich Maria Remarque", 1929},
+	}
+
+	book_repository := repositories.NewBookRepository()
+
+	_, err := book_repository.Create(books)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully Create books")
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func ListOfBooks(w http.ResponseWriter, r *http.Request) {
+
+	initial_books := []entities.Book{
+		{"Rage", "Stephen King", 1977},
+		{"Philosopher's Stone", "J. K. Rowling", 1997},
+		{"All Quiet on the Western Front", "Erich Maria Remarque", 1929},
+	}
+
+	book_repository := repositories.NewBookRepository()
+
+	_, err := book_repository.Create(initial_books)
+	if err != nil {
+		panic(err)
+	}
+
+	books, err := book_repository.List()
+	if err != nil {
+		panic(err)
+	}
+
+	sort.Sort(entities.ByDate(books))
+
+	res, err := json.Marshal(books)
+	if err != nil {
+		panic(err)
+	}
+
+	// output as json file
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+	w.Write(res)
+}
