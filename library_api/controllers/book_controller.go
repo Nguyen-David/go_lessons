18b0606ba34ba823823f6d7ae5a0509c6490949d
devidan <devidan@syneforge.com>
2023-02-20 19:50:47 +0200

    create books from http request

--- library_api/controllers/book_controller.go
+++ library_api/controllers/book_controller.go
@@ -7,26 +7,38 @@ import (
 	"library_api/repositories"
 	"net/http"
 	"sort"
+	"strconv"
 )
 
 func CreateBooks(w http.ResponseWriter, r *http.Request) { // TODO discuss about create action
+	name := r.FormValue("name")
+	author := r.FormValue("author")
+	year, err := strconv.Atoi(r.FormValue("year"))
 
-	books := []entities.Book{
-		{"Rage", "Stephen King", 1977},
-		{"Philosopher's Stone", "J. K. Rowling", 1997},
-		{"All Quiet on the Western Front", "Erich Maria Remarque", 1929},
-	}
-
-	book_repository := repositories.NewBookRepository()
-
-	_, err := book_repository.Create(books)
 	if err != nil {
+		fmt.Println("year incorrect")
+		w.WriteHeader(http.StatusBadRequest)
 		panic(err)
 	}
 
-	fmt.Println("Successfully Create books")
+	if name == "" || author == "" {
+		fmt.Println("name or author incorrect")
+		w.WriteHeader(http.StatusBadRequest)
+	} else {
+		books := []entities.Book{
+			{name, author, year},
+		}
 
-	w.WriteHeader(http.StatusCreated)
+		book_repository := repositories.NewBookRepository()
+
+		_, err := book_repository.Create(books)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Successfully Create books")
+		w.WriteHeader(http.StatusCreated)
+	}
 }
 
 func ListOfBooks(w http.ResponseWriter, r *http.Request) {
@@ -51,7 +63,7 @@ func ListOfBooks(w http.ResponseWriter, r *http.Request) {
 
 	sort.Sort(entities.ByDate(books))
 
-	res, err := json.Marshal(books)
+	res, err := json.Marshal(books) // TODO think how convert year
 	if err != nil {
 		panic(err)
 	}
