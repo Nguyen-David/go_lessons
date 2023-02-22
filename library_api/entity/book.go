d3381d982cf51dffa7dfbfaa76ea67703888c63c
devidan <devidan@syneforge.com>
2023-02-22 22:51:05 +0200

    update marshal json in entity

--- library_api/entity/book.go
+++ library_api/entity/book.go
@@ -1,5 +1,10 @@
 package entity
 
+import (
+	"encoding/json"
+	"strconv"
+)
+
 type Book struct {
 	Name   string `json:"name"`
 	Author string `json:"author"`
@@ -11,3 +16,34 @@ type ByYear []Book
 func (a ByYear) Len() int           { return len(a) }
 func (a ByYear) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByYear) Less(i, j int) bool { return a[i].Year < a[j].Year }
+
+func (b *Book) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&struct {
+		Name   string `json:"name"`
+		Author string `json:"author"`
+		Year   string `json:"year"`
+	}{
+		Name:   b.Name,
+		Author: b.Author,
+		Year:   strconv.Itoa(b.Year),
+	})
+}
+
+func (b *Book) UnmarshalJSON(data []byte) error {
+	book := &struct {
+		Name   string `json:"name"`
+		Author string `json:"author"`
+		Year   string `json:"year"`
+	}{}
+
+	if err := json.Unmarshal(data, book); err != nil {
+		return err
+	}
+
+	var err error
+	b.Name = book.Name
+	b.Author = book.Author
+	b.Year, err = strconv.Atoi(book.Year)
+
+	return err
+}
