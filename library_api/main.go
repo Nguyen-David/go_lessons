7effb568c7847282ffa416d79b8c3eb934b37184
devidan <devidan@syneforge.com>
2023-04-24 22:08:48 +0300

    add docker-compose to library api

--- library_api/main.go
+++ library_api/main.go
@@ -5,11 +5,23 @@ import (
 	"library_api/router"
 	"log"
 	"os"
+	"fmt"
+
+	"gorm.io/gorm"
+  	"gorm.io/driver/postgres"
 )
 
 
 func main() {
-	_, err := config.NewJsonConfig("config.json")
+	dsn := "host=postgres user=myuser password=mypassword dbname=mydatabase port=5432 sslmode=disable"
+	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println("Successfully connected to database!")
+
+	_, err = config.NewJsonConfig("config.json")
 	if err != nil {
 		log.Println(err)
 		return
